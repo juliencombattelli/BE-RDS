69fd72cc80f3f49659cf6b53989d2af941b52e50
ssupaero <student@isae.fr>
2017-12-05 14:01:04 +0100

    [ADD] robot
    Signed-off-by: ssupaero <guillaum324bis@hotmail.fr>

--- tp1/tp0.py
+++ tp1/tp0.py
@@ -10,7 +10,7 @@ import gepetto.corbaserver
 # Example of creation of an object of class Display (implemented in the previous example, inside
 #  a file 'display.py'). Do not forget to start Gepetto-viewer server in another terminal before
 #  creating the client.
-from display import Display
+"""from display import Display
 
 display = Display()
 
@@ -32,6 +32,22 @@ display.viewer.gui.addCylinder('world/cylinder', radius,height,color)
 # Example of use of the class display to place the previously-create object at random SE3 placements.
 display.place("world/box147",se3.SE3.Random(),False)
 display.place("world/sphere",se3.SE3.Random(),False)
-display.place("world/cylinder",se3.SE3.Random())
+display.place("world/cylinder",se3.SE3.Random())"""
+
+# Example of creation of a simple robot model with one single revolute joint rotating around axis X.
+model = se3.Model.BuildEmptyModel()
+jointName          = "first_joint"                       # Name of joint.
+jointPlacement     = se3.SE3.Random()                    # SE3 placement of the joint wrt chain init.
+parent             = 0                                   # Index of the parent (0 is the universe).
+jointModel         = se3.JointModelRX()                  # Type of the joint to be created.
+jointId = model.addJoint(parent,jointModel,jointPlacement,jointName)
+print('Model dimensions: {:d}, {:d}, {:d}'.format(model.nq,model.nv,model.nbodies))
+
+bodyInertia        = se3.Inertia.Random()                # Body mass/center of mass/inertia
+model.appendBodyToJoint(jointId, bodyInertia, body_placement = se3.SE3.Identity()) # The last
+                                                         # argument is likely to always be Identity.
+
+# Example of how to create a 'Data' object from a 'Model' object.
+data = model.createData()
 
 
